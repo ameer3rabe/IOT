bca64f8de6a834b1f57172f194131c3c0348ccd2
ameer3rabe <140540940+ameer3rabe@users.noreply.github.com>
2024-08-04 23:11:44 +0300

    Add files via upload
     קודו למדידת משך לחיצה על כפתור, 
    שבו כשמשך הלחיצה קצר יותר מהקצר ביותר שנמדד עד כה, נורית
     RGB מוארת בצבע טורקיז,
     ואם היא ארוכה יותר
    , נורית RGB מוארת בצבע כתום.

--- sketch_aug2a.ino
+++ sketch_aug2a.ino
@@ -1,23 +1,29 @@
 #define btnPin D5
+#define pinLed_R D1
+#define pinLed_G D2
+#define pinLed_B D3
 
 int lastVal;
 unsigned long pressStartTime;
 unsigned long pressTime;
 bool buttonPressed = false;
+unsigned long shortestPressTime = 500;
+const unsigned long debounceDelay = 50;
 
 void setup() {
   Serial.begin(9600);
   pinMode(btnPin, INPUT_PULLUP);
-  lastVal = digitalRead(btnPin);
+  pinMode(pinLed_R, OUTPUT);
+  pinMode(pinLed_G, OUTPUT);
+  pinMode(pinLed_B, OUTPUT);
 }
 
 void loop() {
   int val = digitalRead(btnPin);
-
-
-  if ((val == LOW) && (lastVal == HIGH) && !buttonPressed) {
+  if ((val == LOW) && (lastVal == HIGH) && !buttonPressed && (millis() - pressStartTime > debounceDelay)) {
     pressStartTime = millis();
     buttonPressed = true;
+    Serial.println("Button Press Started");
   }
 
   if ((val == HIGH) && (lastVal == LOW) && buttonPressed) {
@@ -25,7 +31,23 @@ void loop() {
     buttonPressed = false;
     Serial.print("Press Time: ");
     Serial.println(pressTime);
-  }
 
+    if (pressTime < shortestPressTime) {
+      shortestPressTime = pressTime;
+
+      digitalWrite(pinLed_R, HIGH);
+      digitalWrite(pinLed_G, HIGH);
+      digitalWrite(pinLed_B, HIGH);
+      Serial.println("LED Color: Turquoise");
+    } else {
+
+      digitalWrite(pinLed_R, HIGH);
+      digitalWrite(pinLed_G,HIGH);
+      digitalWrite(pinLed_B, LOW);
+      Serial.println("LED Color: Orange");
+    }
+    Serial.println("Button Press Ended");
+  }
+Serial.println(shortestPressTime);
   lastVal = val;
 }
