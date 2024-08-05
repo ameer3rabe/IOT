3948799fc3d80b93116951b8400caf8fdc317a0b
ameer3rabe <140540940+ameer3rabe@users.noreply.github.com>
2024-08-05 19:52:10 +0300

    Add files via upload
    שינית את הקוד כדי לכלול שליחה וקבלה של נתונים מהשרת,
     כולל שליחת זמן הלחיצה והשוואתו לזמן הקצר ביותר שנשמר בשרת.
     RGP שינויים נוספים כוללים התעדכנות של מצב ה
    בהתבסס על השוואת הזמן והוספת פונקציה לחיבור לרשת

---
+++ ServerInteraction.h
@@ -0,0 +1,72 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
+
+const char* ssid = "Ameer";
+const char* pswd = "0522485513";
+const int deviceId = 1121;
+const int channel = 1;
+
+WiFiClient client;
+
+void wifiClient_Setup() {
+  Serial.println("wifiSetup");
+  WiFi.begin(ssid, pswd);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("trying ...");
+    delay(100);
+  }
+  Serial.println("Connected to network");
+}
+
+String sendPressTimeToServer(unsigned long pressTime) {
+  String serverUrl = "http://api.kits4.me/GEN/api.php?ACT=SET&DEV=" + String(deviceId) + "&CH=" + String(channel) + "&VAL=" + String(pressTime);
+  String response = "";
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(client, serverUrl);
+
+    int httpCode = http.GET();
+
+    if (httpCode > 0) {
+      response = http.getString();
+      Serial.println("Server Response: " + response);
+    } else {
+      Serial.println("HTTP GET failed");
+      response = "Error";
+    }
+    http.end();
+  } else {
+    Serial.println("WiFi not connected");
+    response = "Error";
+  }
+
+  return response;
+}
+
+String getShortestPressTimeFromServer() {
+  String serverUrl = "http://api.kits4.me/GEN/api.php?ACT=GET&DEV=" + String(deviceId) + "&CH=" + String(channel);
+  String response = "";
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(client, serverUrl);
+
+    int httpCode = http.GET();
+
+    if (httpCode > 0) {
+      response = http.getString();
+      Serial.println("Server Response: " + response);
+    } else {
+      Serial.println("HTTP GET failed");
+      response = "Error";
+    }
+    http.end();
+  } else {
+    Serial.println("WiFi not connected");
+    response = "Error";
+  }
+
+  return response;
+}
--- sketch_aug2a.ino
+++ sketch_aug2a.ino
@@ -7,12 +7,12 @@ int lastVal;
 unsigned long pressStartTime;
 unsigned long pressTime;
 bool buttonPressed = false;
-unsigned long shortestPressTime = 2000000000;
 const unsigned long debounceDelay = 50;
 const unsigned long ledOnDuration = 2000;
 
 void setup() {
   Serial.begin(9600);
+  wifiClient_Setup();
   pinMode(btnPin, INPUT_PULLUP);
   pinMode(pinLed_R, OUTPUT);
   pinMode(pinLed_G, OUTPUT);
@@ -33,23 +33,25 @@ void loop() {
     Serial.print("Press Time: ");
     Serial.println(pressTime);
 
-    if (pressTime < shortestPressTime) {
-      shortestPressTime = pressTime;
+    String currentShortestTimeStr = getShortestPressTimeFromServer();
+    unsigned long currentShortestTime = currentShortestTimeStr.toInt();
 
+    if (pressTime < currentShortestTime || currentShortestTime == 0) {
+      String response = sendPressTimeToServer(pressTime);
+        digitalWrite(pinLed_R, HIGH);
+        digitalWrite(pinLed_G, HIGH);
+        digitalWrite(pinLed_B, HIGH);
+        Serial.println("LED Color: Turquoise");
+     
+    } else {
       digitalWrite(pinLed_R, HIGH);
       digitalWrite(pinLed_G, HIGH);
-      digitalWrite(pinLed_B, HIGH);
-      Serial.println("LED Color: Turquoise");
-    } else {
-
-      digitalWrite(pinLed_R, HIGH);
-      digitalWrite(pinLed_G,HIGH);
       digitalWrite(pinLed_B, LOW);
       Serial.println("LED Color: Orange");
     }
-    delay(ledOnDuration);
 
-    digitalWrite(pinLed_R, LOW); 
+    delay(ledOnDuration);
+    digitalWrite(pinLed_R, LOW);
     digitalWrite(pinLed_G, LOW);
     digitalWrite(pinLed_B, LOW);
     Serial.println("Button Press Ended");
