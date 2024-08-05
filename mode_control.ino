cc616cf86c1cc2d66aee89f3af27d4b723fdb014
ameer3rabe <140540940+ameer3rabe@users.noreply.github.com>
2024-08-05 21:36:55 +0300

    Add files via upload
     ,הוספתי טאגל לשליטה במצב 
    הקוד בודק את מצב הפין D7
     אם הוא נמוך, הוא מפעיל את הפונקציה
    need work

---
+++ mode_control.ino
@@ -0,0 +1,26 @@
+#define SETTING_PIN D7
+bool IhaveLed;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(SETTING_PIN, INPUT_PULLUP);
+  IhaveLed = (digitalRead(SETTING_PIN) == LOW) ? true : false;
+
+  if (IhaveLed) {
+    WifiSetup();
+  } else {
+    btn_setup();
+  }
+}
+
+void loop() {
+  if (digitalRead(SETTING_PIN) == LOW) {
+    if (!IhaveLed) {
+      WifiSetup();
+      IhaveLed = true;
+    }
+    wifi_loop(); 
+  } else {
+    btn_loop();
+  }
+}
