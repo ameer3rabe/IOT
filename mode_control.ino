abf62cdab12f71e713acee55bf62dc228d8d2077
ameer3rabe <140540940+ameer3rabe@users.noreply.github.com>
2024-08-06 17:06:12 +0300

    Update mode_control.ino
    removed changed to sketch_aug2a.ino

--- mode_control.ino
+++ mode_control.ino
@@ -1,26 +1,31 @@
-#define SETTING_PIN D7
-bool IhaveLed;
+//xxxxxxxxxxxxxxxxxxxxxTHIS CODE IS NOT NEEDED ANY MORE ITS CHANGED TO SKETCH_AUG2A.INO/xxxxxxxxxxxxxxxxxxxx
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(SETTING_PIN, INPUT_PULLUP);
-  IhaveLed = (digitalRead(SETTING_PIN) == LOW) ? true : false;
 
-  if (IhaveLed) {
-    WifiSetup();
-  } else {
-    btn_setup();
-  }
-}
 
-void loop() {
-  if (digitalRead(SETTING_PIN) == LOW) {
-    if (!IhaveLed) {
-      WifiSetup();
-      IhaveLed = true;
-    }
-    wifi_loop(); 
-  } else {
-    btn_loop();
-  }
-}
+
+//#define SETTING_PIN D7
+//bool IhaveLed;
+
+//void setup() {
+//  Serial.begin(9600);
+//  pinMode(SETTING_PIN, INPUT_PULLUP);
+//  IhaveLed = (digitalRead(SETTING_PIN) == LOW) ? true : false;
+
+//  if (IhaveLed) {
+//    WifiSetup();
+//  } else {
+//    btn_setup();
+//  }
+//}
+
+//void loop() {
+//  if (digitalRead(SETTING_PIN) == LOW) {
+ //   if (!IhaveLed) {
+  //    WifiSetup();
+   //   IhaveLed = true;
+ //   }
+//    wifi_loop(); 
+//  } else {
+ //   btn_loop();
+ // }
+//}
